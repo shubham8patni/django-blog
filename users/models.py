9f259b7237a4b7a362a5d96bf87c759199e9b481
shubham8patni <shubhampatni88@gmail.com>
2022-08-27 03:11:44 +0530

    update user profile, make update profile and update user models, overwrite save() function

--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from PIL import Image
 # Create your models here.
 
 class Profile(models.Model):
@@ -9,3 +9,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.user.username} Profile"
+
+
+    def save(self):   # not the effcicient way to overwrite save function for large projects 
+        super().save()
+
+
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:
+            output = (300,300)
+            img.thumbnail(output)
+            img.save(self.image.path)
